7b1e284b7dd9b1e10c97ea19b037cec543ff813f
Jan Gärtner <jan.gaertner@tum.de>
2020-03-04 15:41:39 +0100

    Update

--- panda-controller/install_nuc.sh
+++ panda-controller/install_nuc.sh
@@ -3,14 +3,42 @@ cd
 # Install ninja
 sudo apt-get install ninja-build
 
+# Install boost
+sudo apt-get install libboost-all-dev
+
 # Install yaml-cpp
 git clone https://github.com/jbeder/yaml-cpp.git
 cd yaml-cpp
 mkdir build
 cd build
-cmake -G Ninja ..
+cmake ..
 sudo make install
 cd
 rm -r -f yaml-cpp
 
 
+
+QT Creator:
+
+sudo apt-get install build-essential
+sudo apt-get install qtcreator
+sudo apt-get install qt5-default
+
+sudo apt-get install git
+git clone git@github.com:TheGaertner/cryopreservation-panda.git
+
+
+# Libfranka
+sudo apt install build-essential cmake git libpoco-dev libeigen3-dev
+git clone --recursive https://github.com/frankaemika/libfranka
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build .
+sudo make install
+
+
+
+
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
--- user-interface/install_interface.sh
+++ user-interface/install_interface.sh
@@ -1,5 +1,9 @@
 cd
 
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+
+sudo apt-get install python-dev python-numpy libjpeg-dev libpng-dev
+
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D OPENCV_GENERATE_PKGCONFIG=ON \
       -D CMAKE_INSTALL_PREFIX=/usr/local \
